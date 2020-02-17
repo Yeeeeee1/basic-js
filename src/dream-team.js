6544e07adf8ca74ae0fbc70571369b1a9b1b7eb5
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-02-17 18:46:10 +0300

    Complete the dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var res = [];
+  if (!members) { return false }
+  if (typeof members != "object") { return false }
+  if (members.isArray == false) { return false }
+  for (let i = 0; i < members.length; i++){
+    if (typeof members[i] == "string"){
+      if (members[i].length > 0){
+      res.push(members[i].trim()[0].toUpperCase());
+      }
+    }
+  }
+  return res.sort().join("");
 };
