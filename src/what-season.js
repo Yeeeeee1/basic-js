268b61565e20082526f70e1307a2c8c5abbc438e
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-02-18 18:33:19 +0300

    Complete the what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == undefined) return 'Unable to determine the time of year!';
+  if (date && date instanceof Date && Object.getOwnPropertyNames(date).length == 0) {
+    var month = date.getMonth();
+    if (month < 2 || month == 11) {
+      return 'winter';
+    }
+    else if (month < 5) {
+      return 'spring';
+    }
+    else if (month < 8) {
+      return 'summer';
+    }
+    else {
+      return 'autumn';
+    }
+  }
+  else{
+    throw new Error();
+  }
 };
