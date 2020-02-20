c956df469c840d59e76f02b35930a69bd65b69aa
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-02-20 15:38:43 +0300

    Complete the extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    var repeatTimes = options.repeatTimes || 1;
+    var separator = options.separator || "+";
+    var addition = options.addition !== undefined ? options.addition : "";
+    var additionRepeatTimes = options.additionRepeatTimes || 1;
+    var additionSeparator = options.additionSeparator || "|";
+  
+    var resultAddition = "";
+    for (var i = 1; i < additionRepeatTimes; i++) {
+      resultAddition = `${resultAddition}${addition}${additionSeparator}`;
+    }
+    resultAddition += addition;
+  
+    var resultString = "";
+    for (var i = 1; i < repeatTimes; i++) {
+      resultString = `${resultString}${str}${resultAddition}${separator}`;
+    }
+    resultString += `${str}${resultAddition}`;
+  
+    return resultString;
 };
   
