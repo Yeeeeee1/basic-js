33805f7cadd5ed68f2d555db1479c9e9d79a4d98
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2020-10-29 22:52:40 +0300

    feat: completed hanoi tower]

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     var turns = 2**disksNumber - 1;
-    var seconds = turns / (turnsSpeed / 3600);
+    var seconds = Math.floor(turns / (turnsSpeed / 3600));
     return {  turns, seconds };
 }
